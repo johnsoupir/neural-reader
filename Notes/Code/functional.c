b4fdddf3346452fe38421d957e0bf1b96c6178e4
John Soupir <johnsoupir@gmail.com>
2023-09-12 10:41:12 -0500

    Adds seedRandom to functional.c

--- Notes/Code/functional.c
+++ Notes/Code/functional.c
@@ -6,11 +6,16 @@
 
 
 
-const float EPS 0.2			//Learning rate
-const float ERROR 0.0001	//Target ERROR
+float const EPS = 0.2;		//Learning rate
+float const ERROR = 0.0001;	//Target ERROR
 
 
-
+uint8_t INPUT[4][2] = {
+	{1,1},
+	{1,0},
+	{0,1},
+	{0,0}
+};
 
 
 
@@ -21,13 +26,67 @@ double sigma(double input)
 }
 
 
-seedRandom()
+void seedRandom(uint8_t * array, uint32_t rows, uint32_t cols, uint32_t layers)
 {
 	srand(time(NULL));
 
+	if(layers > 1)
+	{
+		//3D Array
+		for(uint32_t layerIndex=0; layerIndex<layers; layerIndex++)
+		{
+			for(uint32_t columnIndex=0; columnIndex<cols; columnIndex++)
+			{
+				for(uint32_t rowIndex=0; rowIndex<rows; rowIndex++)
+				{
+					//FILL ARRAY
+//					*((arr + i*y*z) + (j*z) + k) = 0;
+//					*((arr + i*cols) + j) = (rand()%1000)/1000.0;
+	
+				}
+			}
+		}
+	}
+	else
+	{
+		//2D array
+		for(uint32_t columnIndex=0; columnIndex<=cols; columnIndex++)
+		{
+			for(uint32_t rowIndex=0; rowIndex<rows; rowIndex++)
+			{
+				//FILL ARRAY
+				*((array + columnIndex*cols) + rowIndex) = (rand()%1000)/1.0;
 
+			}
+		}
+	}
 }
 
 
+int main()
+{
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<2; j++)
+		{
+			printf("%d\t",INPUT[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("Seeding random\n");
 
+	seedRandom((uint8_t *)INPUT,4,2,1);
+
+	for(int i=0; i<4; i++)
+	{
+		for(int j=0; j<2; j++)
+		{
+			printf("%d\t",INPUT[i][j]);
+		}
+		printf("\n");
+	}
+
+
+}
 
